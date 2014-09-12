3b5034157a9d0a3cd3e9a516c9e53da98df3402f
rengenin <rengenin@gmail.com>
2014-09-12 15:11:45 -0400

    data cheat sheet

---
+++ ingest_basics.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+'''Just a reminder to my self on basic data ingestion (VERY BASIC)'''
+
+'''ingesting text based data'''
+#White space separated variables
+ssv = np.array([float(f) for f in file('space_del_data.txt').read().split()])
+#Comma separated
+csv = np.array([float(f) for f in file('csv_data.txt').read().split(",")])
+
+
+'''ingesting wav file'''
+from scipy.io.wavfile import read
+wav_file = read('test.wav')
+
+#Break out different data parts
+sampling_rate = wav_file[0]
+wav_array = wav_file[1]
